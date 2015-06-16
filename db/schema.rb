1f04b83e4234a206397dd5fdb14a07ba7b30ec59
Valentino Espinoza <xvalentino13@gmail.com>
2015-06-16 10:00:28 -0600

    added board id to ticket model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616155614) do
+ActiveRecord::Schema.define(version: 20150616155833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150616155614) do
     t.integer  "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "board_id"
   end
 
 end
