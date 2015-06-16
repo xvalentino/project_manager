f021dc5543f552f9aae61051a256b4b5a0ba871f
Valentino Espinoza <xvalentino13@gmail.com>
2015-06-16 10:56:05 -0600

    user can now add a ticket onto thier board

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,8 @@
 class TicketsController < ApplicationController
+  def create
+    board_id = params['ticket']['board_id']
+    Ticket.create(title: params['ticket']['title'], board_id: board_id)
+    redirect_to board_path(board_id)
+  end
+
 end
