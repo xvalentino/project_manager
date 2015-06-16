015b81b16e20be57733d7c086f38d6692e433271
Valentino Espinoza <xvalentino13@gmail.com>
2015-06-16 10:07:06 -0600

    ticket now has to have a title

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,4 @@
 class Ticket < ActiveRecord::Base
+  validates :title, presence: true
   belongs_to :board
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "must have a title" do
+    expect(Ticket.new).to_not be_valid
+    expect(Ticket.new(title: "good")).to be_valid
+  end
+
 end
