053d9754b781f35600dcf8c55683f73abe48084b
Valentino Espinoza <xvalentino13@gmail.com>
2015-06-16 09:42:36 -0600

    wrote a spec to test that a baord is created from the index page

--- spec/features/user_can_see_boards_spec.rb
+++ spec/features/user_can_see_boards_spec.rb
@@ -1,9 +1,19 @@
 require "rails_helper"
 
-RSpec.feature "User can see boards", :type => :feature do
-  scenario "on root path" do
+RSpec.feature "User can", :type => :feature do
+  scenario "see index of boards on root path" do
     board = Board.create(title: "first project")
     visit root_path
     expect(page).to have_text(board.title)
+  end
+
+  scenario "create a board on the index page" do
+    expect(Board.all.count).to eq(0)
+    board = {title: "first project"}
+    visit root_path
+    fill_in "Title", with: board[:title]
+    click_link_or_button "Create Board"
+    expect(page).to have_text(board[:title])
+    expect(Board.all.count).to eq(1)
   end
 end
